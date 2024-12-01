d69100ba133de30aa8f0137ff000008ef936915c
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-12-01 15:32:30 -0800

    Reverted-console.log to verify connection to server

--- express_server.js
+++ express_server.js
@@ -9,7 +9,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
