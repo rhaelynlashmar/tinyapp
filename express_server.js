f256e1069ec749c9ed74d618364e21144444d293
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-12-01 20:10:12 -0800

    Added explanatory comments, and generateRandomString() function

--- express_server.js
+++ express_server.js
@@ -13,21 +13,21 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+// Parses data to make body readable
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
+// Adds a table template for URL data
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//Adds a page with a submission form to make a new shortened URL from a longUrl
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// Adds an URLs page with the ids of all shortened URLs and their respective longURL
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
