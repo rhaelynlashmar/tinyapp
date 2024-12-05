bc5124a7afa386ffa128c5906219acebff70aa21
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-12-05 12:21:20 -0800

    Added username cookie to all templates that include partial header

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
@@ -18,18 +19,28 @@ app.use(express.urlencoded({ extended: true }));
 
 // Adds a table template for URL data
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username:req.cookies["username"], //fetch username from cookies
+   };
   res.render("urls_index", templateVars);
 });
 
 //Adds a page with a submission form to make a new shortened URL from a longUrl
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"] 
+  };
+  res.render("urls_new", templateVars);
 });
 
 // Adds an URLs page with the ids of all shortened URLs and their respective longURL
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"],
+  };
   res.render("urls_show", templateVars);
 });
 
